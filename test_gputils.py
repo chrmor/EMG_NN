df12e79cc216e85801d35a46b64eb70444bd9597
Christian Morbidoni <christian.morbidoni@gmail.com>
2018-12-19 15:51:22 +0100

    test GPUtils

--- test_gputils.py
+++ test_gputils.py
@@ -6,9 +6,9 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 try:
     deviceIDs = GPUtil.getAvailable(order='memory', limit=1, maxLoad=100, maxMemory=20)  # return a list of available gpus
 
-    except:
-        print('GPU not compatible with NVIDIA-SMI')
+except:
+    print('GPU not compatible with NVIDIA-SMI')
 
-        else:
-            print(deviceIDs[0])
-            os.environ["CUDA_VISIBLE_DEVICES"] = str(deviceIDs[0])
+    else:
+        print(deviceIDs[0])
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(deviceIDs[0])
